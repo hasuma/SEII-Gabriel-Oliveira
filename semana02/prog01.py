367f9368e3189a0d6a19719d76062260d5b83105
Gabriel Oliveira <gabrielaltamira2013@hotmail.com>
2021-03-13 12:08:52 -0300

    Primeiro codigo - Semana02

--- semana02/prog01.py
+++ semana02/prog01.py
@@ -1,5 +1,38 @@
-import matplotlib as plt
-import numpy as np
-import math as math
+# Working with textual Data
 
+
+#----------------------------------------
 print("Hello World")
+#----------------------------------------
+message = "Hello World"
+print(message)
+#----------------------------------------
+message = "Bobby\'s World"
+print(message)
+#----------------------------------------
+message = "Hello World"
+print(len(message))
+print(message[0])
+print(message[0:5])
+print(message[6:])
+print(message.lower())
+print(message.upper())
+print(message.count("l"))
+print(message.find("World"))
+print(message.find("Palavra que não esta no texto"))
+message.replace("World","Palavra que não esta no texto")
+print(message)
+greeting = "Hello"
+name = "Gabriel"
+message = greeting + ", "+ name + ". Welcome!"
+print(message)
+message = "{}, {}. Welcome!".format(greeting,name)
+print(message)
+message = f"{greeting}, {name}. Welcome!"
+print(message)
+message = f"{greeting}, {name.upper()}. Welcome!"
+print(message)
+#print(dir(name))
+#print(help(str))
+#print(help(str.lower))
+
