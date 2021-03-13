1116053636e0f49a7ada86712a148b0a117e1f59
Gabriel Oliveira <gabrielaltamira2013@hotmail.com>
2021-03-13 12:53:01 -0300

    Segundo codigo - Semana02

---
+++ semana02/prog02.py
@@ -0,0 +1,70 @@
+# Working with Numeric Data
+# -------------------------------------------------------------
+# Arithmetic Operators:
+# Addition:         a + b
+# Subtraction:      a - b
+# Multiplication:   a * b
+# Division:         a / b
+# Floor Diviaion:   a // b
+# Exponent:         a ** b
+# Modulus:          a % b
+# -------------------------------------------------------------
+# Short commands:
+# a = a + b     or  a += b
+# a = a - b     or  a -= b
+# a = a * b     or  a *= b
+# a = a / b     or  a /= b
+# a = a // b    or  a //= b
+# a = a ** b    or  a **= b
+# a = a % b     or  a %= b
+# -------------------------------------------------------------
+# Comparisons:
+# Equal:            a == b
+# Not Equal:        a != b 
+# Greater Than:     a > b
+# Less Than:        a < b
+# Greater or Equal: a >= b
+# Less or Equal:    a <= b
+# -------------------------------------------------------------
+a = 13
+b = 5
+c = 5
+print("-------------------------------------------------------------")
+print(type(a))
+print("-------------------------------------------------------------")
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
+print(a % b)
+print("-------------------------------------------------------------")
+print(a * b - c)
+print(a * (b - c))
+print("-------------------------------------------------------------")
+print(abs(-a))
+print(round(a+0.75))
+print(round(a+0.75,1))
+print("-------------------------------------------------------------")
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print("-------------------------------------------------------------")
+a = "100"
+b = "200"
+print(a + b)
+a = int(a)
+b = int(b)
+print(a + b)
+
+
+
+
+
+
+
+
