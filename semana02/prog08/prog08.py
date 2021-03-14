5985737f5f8521fa7522a91a6cd56bb0484563b9
Gabriel Oliveira <gabrielaltamira2013@hotmail.com>
2021-03-13 21:05:11 -0300

    Oitavo codigo - Semana02

---
+++ semana02/prog08/prog08.py
@@ -0,0 +1,44 @@
+# Import Modules and Exploring The Standard
+
+# import my_module
+# import my_module as mm
+# from my_module import *
+
+# import sys
+# sys.path.append('~/Documentos/SDM/Atividades/semana02/my_module')
+# from my_module import find_index, test
+
+# import calendar
+# import random
+# import math
+# import datetime
+# import os
+import antigravity
+
+# courses = ['History', 'Math', 'Physics', 'CompSci']
+
+# index = my_module.find_index(courses, 'Math')
+# print(index)
+
+# index = mm.find_index(courses, 'Math')
+# print(index)
+
+# index = find_index(courses, 'Math')
+# print(index)
+# print(test)
+
+# print(sys.path)
+
+# random_course = random.choice(courses)
+# print(random_course)
+
+# rads = math.radians(90)
+# print(rads)
+# print(math.sin(rads))
+
+# today = datetime.date.today()
+# print(today)
+# print(calendar.isleap(2020))
+
+# print(os.getcwd())
+# print(os.__file__)
